988ec1b64f600ee47dca856f393c7fafffcdb058
Tu Nombre <montespierre@gmail.com>
2020-12-26 23:43:49 -0500

    spring

---
+++ src/main/java/com/weimont/app/Caral232Application.java
@@ -0,0 +1,13 @@
+package com.weimont.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Caral232Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Caral232Application.class, args);
+	}
+
+}
