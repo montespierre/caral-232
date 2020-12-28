b0f8a81e6c42f6341dfb4ff038627b02c03e3d06
Tu Nombre <montespierre@gmail.com>
2020-12-27 22:32:39 -0500

    primera tabla

--- src/main/java/com/weimont/app/Caral232Application.java
+++ src/main/java/com/weimont/app/Caral232Application.java
@@ -20,20 +20,16 @@ public class Caral232Application {
 	}
 
 	@Bean
-	ApplicationRunner applicationRunner(ArticuloRepository articuloRepository) {
+	ApplicationRunner applicationRunner(ArticuloRepository articuloRepository, EmpleadoRepository empleadoRepository) {
 		return args -> {
 			articuloRepository.save(new Articulo((long) 1, "Zapato", 12.5, (long) 200));
 			articuloRepository.save(new Articulo((long) 2, "Polo", (double) 14, (long) 500));
 			articuloRepository.save(new Articulo((long) 3, "Camisa", 50.4, (long) 300));
-		};
-	}
-	
-	@Bean
-	ApplicationRunner applicationRunner(EmpleadoRepository empleadoRepository) {
-		return args -> {
 			empleadoRepository.save(new Empleado((long) 1, "Juan", "Vendedor", (long) 232121));
 			empleadoRepository.save(new Empleado((long) 2, "Carlos", "Cajero", (long) 5151));
 			empleadoRepository.save(new Empleado((long) 3, "Jose", "Vendedor", (long) 8888));
 		};
 	}
+	
+	
 }
