0f2f32083f88640115607d361af422043c809505
Tu Nombre <montespierre@gmail.com>
2020-12-27 22:29:14 -0500

    primera tabla

--- src/main/java/com/weimont/app/Caral232Application.java
+++ src/main/java/com/weimont/app/Caral232Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.weimont.app.entity.Articulo;
+import com.weimont.app.entity.Empleado;
 import com.weimont.app.repository.ArticuloRepository;
+import com.weimont.app.repository.EmpleadoRepository;
 
 
 
@@ -23,6 +25,15 @@ public class Caral232Application {
 			articuloRepository.save(new Articulo((long) 1, "Zapato", 12.5, (long) 200));
 			articuloRepository.save(new Articulo((long) 2, "Polo", (double) 14, (long) 500));
 			articuloRepository.save(new Articulo((long) 3, "Camisa", 50.4, (long) 300));
+		};
+	}
+	
+	@Bean
+	ApplicationRunner applicationRunner(EmpleadoRepository empleadoRepository) {
+		return args -> {
+			empleadoRepository.save(new Empleado((long) 1, "Juan", "Vendedor", (long) 232121));
+			empleadoRepository.save(new Empleado((long) 2, "Carlos", "Cajero", (long) 5151));
+			empleadoRepository.save(new Empleado((long) 3, "Jose", "Vendedor", (long) 8888));
 		};
 	}
 }
