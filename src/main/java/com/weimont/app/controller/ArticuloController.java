9cc4202811f8eb2b39bea563a6cdf755c5dabf25
Tu Nombre <montespierre@gmail.com>
2020-12-28 21:29:05 -0500

    tabla detalle de venta

--- src/main/java/com/weimont/app/controller/ArticuloController.java
+++ src/main/java/com/weimont/app/controller/ArticuloController.java
@@ -12,26 +12,25 @@ import com.weimont.app.entity.Articulo;
 import com.weimont.app.service.ArticuloService;
 
 @Controller
-@RequestMapping({"/articulos"})
+@RequestMapping({ "/articulos" })
 public class ArticuloController {
 
 	@Autowired
 	private ArticuloService articuloService;
-	
-	
-	@GetMapping({"/", "/todos"})
+
+	@GetMapping({ "/", "/todos" })
 	public String listar(Model model) {
 		List<Articulo> lista = articuloService.listarTodos();
 		model.addAttribute("titulo", "Articulos");
 		model.addAttribute("articulos", lista);
 		return "articulos/listado";
 	}
-	
+
 	@GetMapping("/crear")
 	public String crear(Model model) {
-	Articulo articulo = new Articulo();
-	model.addAttribute("titulo", "NUEVO ARTICULO");
-	model.addAttribute("producto", articulo);
-	return "articulos/frmEditar";
+		Articulo articulo = new Articulo();
+		model.addAttribute("titulo", "NUEVO ARTICULO");
+		model.addAttribute("producto", articulo);
+		return "articulos/frmEditar";
 	}
 }
